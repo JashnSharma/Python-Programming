cc73b056211e8bfefe56446f7abc3fce6b883faa
JashnSharma <jashnsharma18@gmail.com>
2024-11-11 11:05:51 +0530

    Update Q48 list comprehensions.py

--- Q48 list comprehensions.py
+++ Q48 list comprehensions.py
@@ -3,7 +3,7 @@ squares = [x**2 for x in range(10)]
 even_squares = [x**2 for x in range(10) if x % 2 == 0]
 print("Squares:", squares)
 print("Even squares:", even_squares)
-print("program by Diya Bhutani- 0221BCA041")
+print("Program by jashn sharma - 0221BCA048")
 
 
 
