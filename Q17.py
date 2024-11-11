d39b69e17433571069fae29d17617688f2f50d61
JashnSharma <jashnsharma18@gmail.com>
2024-11-11 10:54:09 +0530

    Update Q17.py

--- Q17.py
+++ Q17.py
@@ -10,4 +10,4 @@ print(f"{a} or {b} : {a or b}")
 #NOT Operators
 print(f"not {a} :  {not a}")
 print(f"not {b} :  {not b}")
-print("Program by Diya Bhutani - 0221BCA041")
+print("Program by jashn sharma - 0221BCA048")
