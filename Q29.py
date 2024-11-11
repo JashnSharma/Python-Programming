45f772dce7d8b76213c30e426d03d5e7055d4437
JashnSharma <jashnsharma18@gmail.com>
2024-11-11 10:57:26 +0530

    Update Q29.py

--- Q29.py
+++ Q29.py
@@ -3,4 +3,4 @@ num=16
 print(f"square root of {num} is {math.sqrt(num)}")
 print(f"Factorial of {num} is {math.factorial(num)}")
 print(f"Logarithm of {num} is {math.log(num)}")
-print("Program by Diya Bhutani - 0221BCA041")
+print("Program by jashn sharma - 0221BCA048")
