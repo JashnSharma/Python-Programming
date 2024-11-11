be2a8a50d7c9f49bd94582dbfc867272f63218ef
JashnSharma <jashnsharma18@gmail.com>
2024-11-11 11:06:29 +0530

    Update Q50 iterators in a loop.py

--- Q50 iterators in a loop.py
+++ Q50 iterators in a loop.py
@@ -3,6 +3,6 @@ my_list = [1, 2, 3, 4]
 it = iter(my_list)
 for item in it:
  print(item) # Outputs: 1, 2, 3, 4
-print("program by Diya Bhutani- 0221BCA041")
+print("Program by jashn sharma - 0221BCA048")
 
 
