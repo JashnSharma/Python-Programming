038d092f68db3a96e4f547241c86b287b057a5d1
JashnSharma <jashnsharma18@gmail.com>
2024-11-11 11:06:50 +0530

    Update Q52 generator loop.py

--- Q52 generator loop.py
+++ Q52 generator loop.py
@@ -6,5 +6,4 @@ def fibonacci(n):
   a, b = b, a + b
 for num in fibonacci(10):
  print(num) # Outputs: 0, 1, 1, 2, 3, 5, 8
-print("program by Diya Bhutani- 0221BCA041")
-
+print("Program by jashn sharma - 0221BCA048")
